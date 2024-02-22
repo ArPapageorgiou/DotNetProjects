3b1afade858099b619699fba6c1eed7c1a6f7c7e
Argiris <ar.papagewrgiou@gmail.com>
2024-02-22 02:20:43 +0200

    Added new project files

---
+++ ADO.NET Deleting Record/ADO.NET Deleting Record/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Data.SqlClient;
+
+namespace ADO.NET_Deleting_Record
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().DeletingRecord();
+            Console.WriteLine();
+        }
+
+        public void DeletingRecord() 
+        { 
+        
+        SqlConnection conn = null;
+            try
+            {
+
+                conn = new SqlConnection("data source = DESKTOP-3BA0QHV; database = Northwind; integrated security = SSPI ");
+                SqlCommand cm = new SqlCommand("DELETE FROM Promotions WHERE EmployeeID = '2'", conn);
+                conn.Open();
+                cm.ExecuteNonQuery();
+                Console.WriteLine("Command executed successfully");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong: " + ex.Message);
+            }
+            finally
+            {
+                if (conn != null)
+
+                {
+                    conn.Close();
+                }
+            }
+            
+        
+        }
+    }
+}
---
+++ ADO.NET INSERT/ADO.NET INSERT/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Data.SqlClient;
+
+namespace ADO.NET_INSERT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().InsertRecord();
+            Console.ReadLine(); 
+        }
+
+        public void InsertRecord() 
+        { 
+
+        SqlConnection conn = null;
+            try 
+            {
+                conn = new SqlConnection("data source= DESKTOP-3BA0QHV; database = Northwind; integrated security = SSPI;");
+                SqlCommand cm = new SqlCommand("INSERT INTO Promotions(EmployeeID, Name, email, join_date) values(2, 'Fuller', 'fuller@gmail.com', '02-11-2019')", conn);
+                conn.Open();
+                cm.ExecuteNonQuery();
+                Console.WriteLine("Record inserted successfully");
+            }
+            catch (Exception ex) 
+            { 
+            Console.WriteLine("Oooops something went wrong " + ex.Message);   
+            }
+            finally 
+            { 
+                if (conn != null && conn.State == System.Data.ConnectionState.Open) 
+                { 
+                conn.Close();
+                } 
+            }
+
+        }
+    }
+}

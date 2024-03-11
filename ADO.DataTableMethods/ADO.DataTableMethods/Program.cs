128e3789456033efa6f7328e54986de4174d9216
ArPapageorgiou <159928305+ArPapageorgiou@users.noreply.github.com>
2024-03-11 22:45:45 +0200

    Ado data adapter stored procedure with input and output parameters (#5)
    * Added new project
    
    * Added new projects
    
    ---------
    
    Co-authored-by: Argiris <ar.papagewrgiou@gmail.com>

---
+++ ADO.DataTableMethods/ADO.DataTableMethods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+
+namespace ADO.DataTableMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+                string connectionString = "data source=DESKTOP-3BA0QHV; database=StudentDB; integrated security=SSPI";
+                using (SqlConnection connection = new SqlConnection(connectionString)) 
+                {
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter("SELECT * FROM Student", connection);
+                    DataTable dataTable1 = new DataTable();
+                    dataAdapter.Fill(dataTable1);
+
+                    foreach (DataRow row in dataTable1.Rows) 
+                    {
+                        Console.WriteLine(row[0] + " " + row[1] + " " + row[2]);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}

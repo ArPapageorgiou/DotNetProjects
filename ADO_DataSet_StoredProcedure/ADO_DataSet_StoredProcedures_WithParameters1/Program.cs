b4dea7bf7066ed09586aa9d0d1a5d87c0f0ca0af
Argiris <ar.papagewrgiou@gmail.com>
2024-03-13 22:33:33 +0200

    Added Practice_Calling_StoredProcedure_WithInputAndOutputParameters

---
+++ ADO_DataSet_StoredProcedure/ADO_DataSet_StoredProcedures_WithParameters1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+
+namespace ADO_DataSet_StoredProcedures_WithParameters1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+
+                string connectionString = "data source=DESKTOP-3BA0QHV; database=DBStudents; integrated security=SSPI";
+
+
+                using (SqlConnection connection = new SqlConnection(connectionString)) 
+                {
+
+                    SqlCommand command = new SqlCommand("spCreateStudent", connection) 
+                    { 
+                    CommandType = System.Data.CommandType.StoredProcedure
+                    };
+
+                    SqlParameter parameter1 = new SqlParameter
+                    {
+                        ParameterName = "@Name",
+                        SqlDbType = SqlDbType.NVarChar,
+                        Value = "Test",
+                        Direction = ParameterDirection.Input
+                    };
+
+                    command.Parameters.Add(parameter1);
+
+                    SqlParameter parameter2 = new SqlParameter()
+                    { 
+                    ParameterName = "@Email",
+                    SqlDbType=SqlDbType.NVarChar,
+                    Value = "Test@adonet.net",
+                    Direction = ParameterDirection.Input
+                    };
+
+                    command.Parameters.Add(parameter2);
+
+                    SqlParameter parameter3 = new SqlParameter()
+                    { 
+                    ParameterName = "@Mobile",
+                    SqlDbType=SqlDbType.NVarChar,
+                    Value = "1234567890",
+                    Direction = ParameterDirection.Input
+                    };
+
+                    command.Parameters.Add(parameter3);
+
+                    SqlParameter outParameter = new SqlParameter()
+                    {
+                        ParameterName = "@Id",
+                        SqlDbType = SqlDbType.Int,
+                        Direction = ParameterDirection.Output
+                    };
+
+                    command.Parameters.Add(outParameter);
+
+                    connection.Open();
+
+                    command.ExecuteNonQuery();
+
+                    Console.WriteLine("Newly Generated Student ID: " + outParameter.Value.ToString());
+
+                    
+
+                }
+
+
+            }
+			catch (Exception e)
+			{
+                Console.WriteLine("Error: " + e.Message);
+			}
+        }
+    }
+}

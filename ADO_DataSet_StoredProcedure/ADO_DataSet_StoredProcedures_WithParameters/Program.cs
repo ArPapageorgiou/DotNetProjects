12e93074af80a189fce7c44e2afcc3d01d82d302
ArPapageorgiou <159928305+ArPapageorgiou@users.noreply.github.com>
2024-03-13 17:06:44 +0200

    Ado stored procedures (#10)
    * Added new project
    
    * Added new projects
    
    * Added new projects
    
    * Added new project
    
    * repeat/practice of parameterless stored procedure in ADO
    
    * Practice Calling parameterless stored procedures with ADO
    
    * Fixed connection closed exception in StoredProcedures2 and added Calling StoredProcedures_ WithParameter Project
    
    ---------
    
    Co-authored-by: Argiris <ar.papagewrgiou@gmail.com>

--- ADO_DataSet_StoredProcedure/ADO_DataSet_StoredProcedures_WithParameters/Program.cs
+++ ADO_DataSet_StoredProcedure/ADO_DataSet_StoredProcedures_WithParameters/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Data.SqlClient;
 using System.Data;
 
-namespace ADO_DataSet_StoredProcedures2
+namespace ADO_DataSet_StoredProcedures_WithParameters
 {
     internal class Program
     {
@@ -9,33 +9,47 @@ namespace ADO_DataSet_StoredProcedures2
         {
 			try
 			{
-                string connectionString = 
+                string connectionString =
                     "data source=DESKTOP-3BA0QHV; " +
                     "database=StudentDB; " +
                     "integrated security=SSPI";
 
                 using (SqlConnection connection = new SqlConnection(connectionString)) 
                 {
-                    SqlCommand command = new SqlCommand("spGetStudents", connection)
+                    SqlCommand command = new SqlCommand()
                     {
+                        CommandText = "spGetStudentById",
+                        Connection = connection,
                         CommandType = CommandType.StoredProcedure
                     };
 
+                    SqlParameter parameter1 = new SqlParameter
+                    {
+                        ParameterName = "@Id",
+                        SqlDbType = SqlDbType.Int,
+                        Value = 102,
+                        Direction =ParameterDirection.Input
+                    };
+
+                    command.Parameters.Add(parameter1);
 
                     connection.Open();
-  
+
                     SqlDataReader reader = command.ExecuteReader();
 
                     while (reader.Read()) 
                     {
                         Console.WriteLine(reader["Id"] + ",  " + reader["Name"] + ",  " + reader["Email"] + ",  " + reader["Mobile"]);
                     }
+
+                    
                 }
+
 
             }
 			catch (Exception e)
 			{
-                Console.WriteLine("Ooops.. Something went wrong: " + e.Message);
+                Console.WriteLine("Error: " + e.Message);
 			}
         }
     }

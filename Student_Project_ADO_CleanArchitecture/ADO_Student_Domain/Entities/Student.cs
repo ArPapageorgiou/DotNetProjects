348b16d5596977672e54f7c54988b3565c4fe5be
Argiris <ar.papagewrgiou@gmail.com>
2024-03-13 22:49:17 +0200

    Created ADO_StudentDBApplicationProject

---
+++ Student_Project_ADO_CleanArchitecture/ADO_Student_Domain/Entities/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace ADO_Student_Domain.Entities
+{
+    public class Student
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public bool IsCool { get; set; }
+    }
+}

60a5d04b46754b5f6527cd33f3d0da9cc7b11b82
Argiris <ar.papagewrgiou@gmail.com>
2024-03-13 15:40:49 +0200

    repeat/practice of parameterless stored procedure in ADO

---
+++ ADO_DataSet_StoredProcedure/ADO_DataSet_StoredProcedures1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+
+namespace ADO_DataSet_StoredProcedures1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+                string connectionString = 
+                    "data source=DESKTOP-3BA0QHV; " +
+                    "database=StudentDB; " +
+                    "integrated security=SSPI";
+
+                using (SqlConnection connection = new SqlConnection(connectionString)) 
+                {
+
+                    SqlCommand command = new SqlCommand("spGetStudents", connection)
+                    {
+                        CommandType = CommandType.StoredProcedure
+                    };
+
+                    connection.Open();
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read()) 
+                    {
+                        Console.WriteLine(reader["Id"] + ",  " + reader["Name"] + ",  " + reader["Email"] + ",  " + reader["Mobile"]);
+                    }
+
+                }
+
+            }
+			catch (Exception e)
+			{
+                Console.WriteLine("Error: " + e.Message);
+			}
+        }
+    }
+}

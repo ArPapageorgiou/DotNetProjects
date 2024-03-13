4ebf73dc5981fd2fa94d79cffc4840857d383224
ArPapageorgiou <159928305+ArPapageorgiou@users.noreply.github.com>
2024-03-13 15:37:47 +0200

    Ado stored procedures (#7)
    * Added new project
    
    * Added new projects
    
    * Added new projects
    
    * Added new project
    
    ---------
    
    Co-authored-by: Argiris <ar.papagewrgiou@gmail.com>

---
+++ ADO_DataSet_StoredProcedure/ADO_DataSet_StoredProcedure/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+
+namespace ADO_DataSet_StoredProcedure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+                string connectionString =
+                    "data source=DESKTOP-3BA0QHV; " +
+                    "database=StudentDB; " +
+                    "integrated security=SSPI";
+
+                using (SqlConnection connection = new SqlConnection(connectionString)) 
+                {
+
+                    SqlCommand command = new SqlCommand("spGetStudents", connection)
+                    {
+                        CommandType = CommandType.StoredProcedure
+                    };
+
+                    connection.Open();
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read()) 
+                    {
+                        Console.WriteLine(reader[0]+" " + reader[1] + " " + reader[2] + " " + reader[3]);
+                    }
+
+                }
+
+            }
+			catch (Exception e)
+			{
+                Console.WriteLine("Error: " + e.Message);
+			}
+        }
+    }
+}

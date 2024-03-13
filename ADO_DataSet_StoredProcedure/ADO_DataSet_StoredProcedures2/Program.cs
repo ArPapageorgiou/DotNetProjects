41426a7f76cf2bc926f7577bbe19176719011d7f
ArPapageorgiou <159928305+ArPapageorgiou@users.noreply.github.com>
2024-03-13 16:02:35 +0200

    Ado stored procedures (#9)
    * Added new project
    
    * Added new projects
    
    * Added new projects
    
    * Added new project
    
    * repeat/practice of parameterless stored procedure in ADO
    
    * Practice Calling parameterless stored procedures with ADO
    
    ---------
    
    Co-authored-by: Argiris <ar.papagewrgiou@gmail.com>

---
+++ ADO_DataSet_StoredProcedure/ADO_DataSet_StoredProcedures2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+
+namespace ADO_DataSet_StoredProcedures2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+                string connectionString = 
+                    "data source=DESKTOP-3BA0QHV; " +
+                    "database=StudentDB; " +
+                    "integrated security=SSPI";
+
+                using (SqlConnection connection = new SqlConnection(connectionString)) 
+                {
+                    SqlCommand command = new SqlCommand("spGetStudents", connection)
+                    {
+                        CommandType = CommandType.StoredProcedure
+                    };
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read()) 
+                    {
+                        Console.WriteLine(reader["Id"] + ",  " + reader["Name"] + ",  " + reader["Email"] + ",  " + reader["Mobile"]);
+                    }
+                }
+
+            }
+			catch (Exception e)
+			{
+                Console.WriteLine("Ooops.. Something went wrong: " + e.Message);
+			}
+        }
+    }
+}

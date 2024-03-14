ebe2d59658afef8f2681d3f1097972e5b9c2ccd1
Argiris <ar.papagewrgiou@gmail.com>
2024-03-14 21:52:03 +0200

    ADO_DataSet_StoredProcedure_2_CreateCommandVersion

---
+++ ADO_DataSet_StoredProcedure/ADO_DataSet_StoredProcedure_2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace ADO_DataSet_StoredProcedure_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+
+                string connectionString =
+                    "data source=DESKTOP-3BA0QHV; " +
+                    "database=EmployeeDB; " +
+                    "integrated security=SSPI";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand command = connection.CreateCommand();
+
+                    command.CommandText = "spGetEmployeesByAgeDept";
+
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.Parameters.AddWithValue("@Age", 25);
+                    command.Parameters.AddWithValue("@Dept", "IT");
+
+                    DataSet dataSet = new DataSet();
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    
+                    adapter.Fill(dataSet);
+
+                    foreach (DataRow row in dataSet.Tables[0].Rows) 
+                    {
+                        Console.WriteLine(row[0] + " " + row[1] + " " + row[2] + " " + row[3] + " " + row[4] + " " + row[5]);
+                    }
+                }
+
+
+            }
+			catch (Exception e)
+			{
+                Console.WriteLine("Error: " + e.Message);
+			}
+        }
+    }
+}

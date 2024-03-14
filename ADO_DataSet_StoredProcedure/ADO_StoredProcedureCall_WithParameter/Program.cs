f6ed40cfd0d00444c9e58e87418e62328d8948d4
Argiris <ar.papagewrgiou@gmail.com>
2024-03-14 18:03:08 +0200

    StoredProcedureCall_WithParameter_DeleteRows

---
+++ ADO_DataSet_StoredProcedure/ADO_StoredProcedureCall_WithParameter/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+
+
+namespace ADO_StoredProcedureCall_WithParameter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+
+                string connectionString = "data source=DESKTOP-3BA0QHV; database=DBStudents; integrated security=SSPI";
+                
+                int[] isToDelete = { 110, 111, 112 };
+
+                 
+                foreach (int i in isToDelete) 
+                {
+                    
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+
+                        SqlCommand command = new SqlCommand("spDeleteStudentRowsByIdStudent", connection)
+                        {
+                            CommandType = CommandType.StoredProcedure
+                        };
+
+                        command.Parameters.Add(new SqlParameter("@Id", i));
+
+                        connection.Open();
+
+                        command.ExecuteNonQuery();
+
+                        Console.WriteLine($"Deleted row with Id: {i}");
+                    
+                    }
+
+                }
+
+            }
+			catch (Exception e)
+			{
+                Console.WriteLine("Error: " + e.Message);
+			}
+        }
+    }
+}

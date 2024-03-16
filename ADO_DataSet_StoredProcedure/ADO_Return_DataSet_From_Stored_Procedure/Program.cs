5e023e69dc32bfb647af93fcb11c6190707d801f
ArPapageorgiou <159928305+ArPapageorgiou@users.noreply.github.com>
2024-03-16 18:21:38 +0200

    Stored procedure with parameters (#17)
    * StoredProcedureCall_WithParameter_DeleteRows
    
    * added ADO_StoredProcedureCall_WithParameter1and2
    
    * ADO_DataSet_StoredProcedure_2_CreateCommandVersion
    
    * Return Dataset From Stored Procedures using Generic Methods
    
    ---------
    
    Co-authored-by: Argiris <ar.papagewrgiou@gmail.com>

---
+++ ADO_DataSet_StoredProcedure/ADO_Return_DataSet_From_Stored_Procedure/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+
+namespace ADO_Return_DataSet_From_Stored_Procedure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                string connectionString = "data source=DESKTOP-3BA0QHV; database=EmployeeDB; integrated security=SSPI";
+
+                SqlParameter[] parameterList = new SqlParameter[]
+                {
+                new SqlParameter("@Age", 25),
+                new SqlParameter("@Dept", "IT")
+                };
+
+                DataSet mainDataSet = ExecuteStoredProcedureReturnDataSet(connectionString, "spGetEmployeesByAgeDept", parameterList);
+
+                Console.WriteLine("spGetEmployeesByAgeDept Result:");
+                foreach (DataRow row in mainDataSet.Tables[0].Rows)
+                {
+                    Console.WriteLine(row["Id"] + ",  " + row["Name"] + ",  " + row["Email"] + ",  " + row["Mobile"] + ",  " + row["Age"] + ",  " + row["Department"]);
+                }
+
+                Console.WriteLine();
+
+                DataSet mainDataSet2 = ExecuteStoredProcedureReturnDataSet(connectionString, "spGetEmployees");
+
+                Console.WriteLine("spGetEmployees Result:");
+                foreach (DataRow row in mainDataSet2.Tables[0].Rows) 
+                {
+                    Console.WriteLine(row["Id"] + ",  " + row["Name"] + ",  " + row["Email"] + ",  " + row["Mobile"] + ",  " + row["Age"] + ",  " + row["Department"]);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+        }
+
+        public static DataSet ExecuteStoredProcedureReturnDataSet(string connectionString, string procedureName, params SqlParameter[] parameterList)
+        {
+
+            DataSet dataset = new DataSet();
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var command = connection.CreateCommand())
+                {
+
+                    using (var dataAdapter = new SqlDataAdapter(command))
+                    {
+
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.CommandText = procedureName;
+
+                        if (parameterList != null)
+                        {
+
+                            command.Parameters.AddRange(parameterList);
+
+                        }
+
+                        dataAdapter.Fill(dataset);
+
+                    }
+
+                }
+            }
+
+            return dataset;
+
+        }
+    }
+}

d884ecb3bd15ca42b7239d8b4bd3311ccb639976
Argiris <ar.papagewrgiou@gmail.com>
2024-03-19 14:18:13 +0200

    CallingStoredProcedure_withInputParameter

---
+++ ADO_DataSet_StoredProcedure/ADO_UsingStoredProcedure_WithInputParameters/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace ADO_UsingStoredProcedure_WithInputParameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			try 
+			{
+
+                string connectionString = "data source=DESKTOP-3BA0QHV; database=DBStudents; integrated security=SSPI";
+
+                using (SqlConnection connection = new SqlConnection(connectionString)) 
+                { 
+                    SqlCommand command = connection.CreateCommand();
+                    command.CommandText = "spGetStudentById";
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    SqlParameter parameter = new SqlParameter 
+                    { 
+                        ParameterName = "@Id",
+                        SqlDbType = SqlDbType.Int,
+                        SqlValue = 113,
+                        Direction = ParameterDirection.Input,
+                    
+                    };
+                    command.Parameters.Add(parameter);
+
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read()) 
+                    {
+                        Console.WriteLine(reader["Id"] + ",  " + reader["Name"] + ",  " + reader["Email"] + ",  " + reader["Mobile"]);
+                    }
+
+
+                }
+
+
+            }
+			catch (Exception e)
+			{
+                Console.WriteLine ("Error: " + e.Message);
+			}
+        }
+    }
+}

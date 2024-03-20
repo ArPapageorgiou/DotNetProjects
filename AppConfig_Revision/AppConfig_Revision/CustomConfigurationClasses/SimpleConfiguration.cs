5c3831877b436771e66d6ad45f8a86778f5c7cf8
Argiris <ar.papagewrgiou@gmail.com>
2024-03-20 17:53:08 +0200

    AppConfig_Revision

---
+++ AppConfig_Revision/AppConfig_Revision/CustomConfigurationClasses/SimpleConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+
+namespace AppConfig_Revision.CustomConfigurationClasses
+{
+    internal class SimpleConfiguration : ConfigurationSection
+    {
+        [ConfigurationProperty("connectionString")]
+        public string ConnectionString 
+        {
+            get => this["connectionString"].ToString() ?? string.Empty;
+            set => this["connectionString"] = value;
+        }
+
+        [ConfigurationProperty("pageSize")]
+        public int PageSize 
+        {
+            get => Convert.ToInt32(this["pageSize"]);
+            set => this["pageSize"] = value;
+
+        }
+    }
+}

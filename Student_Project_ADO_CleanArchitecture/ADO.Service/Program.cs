88ba2132dbef4a06054573378969e1e4e0d2d3ee
ArPapageorgiou <159928305+ArPapageorgiou@users.noreply.github.com>
2024-03-24 17:58:24 +0200

    Stored procedure with parameters (#34)
    * StoredProcedureCall_WithParameter_DeleteRows
    
    * added ADO_StoredProcedureCall_WithParameter1and2
    
    * ADO_DataSet_StoredProcedure_2_CreateCommandVersion
    
    * Return Dataset From Stored Procedures using Generic Methods
    
    * Updated StudentProject_ADO_CleanArchitecture
    
    * ADO_UsingStoredProcedure and update Student Project
    
    * Added Stored Procedure call with Input and output parameters
    
    * Dependency_Injection_Using_IServiceCollection_IHost_Revision
    
    * Fixed mistake in both Solutions
    
    * AppConfig_Revision
    
    * added ComplexConfigurationsSection mapping
    
    * Added ExtremeCoonfiguration mappings and used CustomConfigClasses in main
    
    * Implemented more StudentRepository methods
    
    * Added  GetAllStudentsWithProcedure method to StudentRepository
    
    * Added new Methods to StudentRepository
    
    * Added more Method implementations sto StudentRepository
    
    * Added new Method Implementations in StudentRepository
    
    * Added new Method Implementations in StudentRepository
    
    * Resolved dependencies for Infrastructure and created application's host in ADO.Service.Program
    
    ---------
    
    Co-authored-by: Argiris <ar.papagewrgiou@gmail.com>

--- Student_Project_ADO_CleanArchitecture/ADO.Service/Program.cs
+++ Student_Project_ADO_CleanArchitecture/ADO.Service/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace ADO.Service
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ADO.Application.Services;
+using ADO.Application.Interfaces;
+using ADO.Infrastructure;
+
+namespace ADO.Service
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            IHost host = Host.CreateDefaultBuilder().ConfigureServices((context, services) =>
+            {
+                services.AppServices();
+                services.InfraServices();
+            }).Build();
+
+            var app = host.Services.GetRequiredService<IApplication>();
+            app.BulkInsertTextRun();
         }
     }
 }

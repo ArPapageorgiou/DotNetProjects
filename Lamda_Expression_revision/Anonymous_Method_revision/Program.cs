606745b0e98c3909ede6b468916bf5ce5565a4aa
Argiris <ar.papagewrgiou@gmail.com>
2024-03-27 16:05:19 +0200

    Added new practice projects for revision purposes.

---
+++ Lamda_Expression_revision/Anonymous_Method_revision/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Anonymous_Method_revision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int sum = SumEvenNumbers(numbers);
+
+            Console.WriteLine("Sum of even numbers: " + sum); 
+
+
+
+        }
+
+        static int SumEvenNumbers(List<int> numbers) 
+        { 
+            int sum = 0;
+            numbers.ForEach(delegate (int num)
+            {
+                if (num % 2 == 0) 
+                {
+                    sum += num;
+                }
+            });
+            return sum;
+
+        }
+    }
+}
---
+++ Lamda_Expression_revision/Delegates_revision/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Delegates_revision
+{
+    internal class Program
+    {
+        delegate double AverageCalculator(List<int> numbers);
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() {1,2,3,4,5};
+            AverageCalculator calculator = CalculateAverage;
+
+            double average = calculator(numbers);
+            Console.WriteLine("Average: " + average);
+        }
+
+        static double CalculateAverage(List<int> numbers) 
+        { 
+            int sum = 0;
+            foreach (int num in numbers) 
+            { 
+            sum += num;
+            }
+
+            double average = sum/numbers.Count;
+
+            return average;
+        }
+    }
+}

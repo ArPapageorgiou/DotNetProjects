2924d477d14d6fd11b377cec34b49b28cf3f0990
Argiris <ar.papagewrgiou@gmail.com>
2024-03-27 15:47:47 +0200

    Lamda_Revision

---
+++ Lamda_Expression_revision/Lamda_Expression_revision/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Lamda_Expression_revision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int> { 1, 4, 6, 7, 9, 12 };
+            List<int> squares = list.ConvertAll(num => num * num);
+
+            Console.WriteLine("Squares of numbers:");
+            foreach (int square in squares) 
+            { 
+            Console.WriteLine(square);
+            }
+        }
+    }
+}

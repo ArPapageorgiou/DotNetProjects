d04288597fb36db4dca81525ade7e2989a99145b
ArPapageorgiou <159928305+ArPapageorgiou@users.noreply.github.com>
2024-03-30 04:27:51 +0200

    Linq (#44)
    * StoredProcedureCall_WithParameter_DeleteRows
    
    * added ADO_StoredProcedureCall_WithParameter1and2
    
    * ADO_DataSet_StoredProcedure_2_CreateCommandVersion
    
    * Return Dataset From Stored Procedures using Generic Methods
    
    * Updated StudentProject_ADO_CleanArchitecture
    
    * ADO_UsingStoredProcedure and update Student Project
    
    * Added Stored Procedure call with Input and output parameters
    
    * Dependency_Injection_Using_IServiceCollection_IHost_Revision
    
    * Fixed mistake in both Solutions
    
    * AppConfig_Revision
    
    * added ComplexConfigurationsSection mapping
    
    * Added ExtremeCoonfiguration mappings and used CustomConfigClasses in main
    
    * Implemented more StudentRepository methods
    
    * Added  GetAllStudentsWithProcedure method to StudentRepository
    
    * Added new Methods to StudentRepository
    
    * Added more Method implementations sto StudentRepository
    
    * Added new Method Implementations in StudentRepository
    
    * Added new Method Implementations in StudentRepository
    
    * Resolved dependencies for Infrastructure and created application's host in ADO.Service.Program
    
    * Added new Bulk Inser & Copy practice project
    
    * Corrected a mistake in the conditiion  of the Application class methods where the conditiion was if(_students is null || _students.Any()) as it always through an exception. Also corrected a mistake in the app.config path to the DataBase Configuration class.
    
    * Revisited Delegates for practice.
    
    * Lamda_Revision
    
    * Added new practice projects for revision purposes.
    
    * Demonstrated the use of LINQ Method Syntax and LINQ Query Syntax in Program
    
    * Demonstrated Usage of Method and LINQ Query syntex for Inner Join in Main
    
    * Added Group Join examples in Main using Method and LINQ Query syntax.
    
    * Created small Examples for OrderBy, GroupBy, ToLookUp operators. Also included examples for All and Any Quantifiers.
    
    * Created example of Contains() method with custom comparer
    
    ---------
    
    Co-authored-by: Argiris <ar.papagewrgiou@gmail.com>

--- LINQ_Operators_Examples/LINQ_Operators_Examples/Program.cs
+++ LINQ_Operators_Examples/LINQ_Operators_Examples/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace LINQ_Operators_Examples
+
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace LINQ_Operators_Examples
+
 {
     internal class Program
     {
@@ -80,34 +84,93 @@
             //----------QUANTIFIERS----------
 
             //All Quantifier
-            var annualSalaryCompare = 20000;
 
-            bool isTrueAll = employeeList.All(e => e.AnnualSalary > annualSalaryCompare);
-            if (isTrueAll)
-            {
-                Console.WriteLine($"All employee Annual Salaries are above {annualSalaryCompare}");
-            }
-            else 
-            { 
-             Console.WriteLine($"Not all employee Annual Salaries are above {annualSalaryCompare}");
-            }
+            //var annualSalaryCompare = 50000;
+
+            //bool isTrueAll = employeeList.All(e => e.AnnualSalary > annualSalaryCompare);
+            //if (isTrueAll)
+            //{
+            //    Console.WriteLine($"All employee Annual Salaries are above {annualSalaryCompare}");
+            //}
+            //else 
+            //{ 
+            // Console.WriteLine($"Not all employee Annual Salaries are above {annualSalaryCompare}");
+            //}
 
             //Any Quantifier
-            bool isTrueAny = employeeList.Any(e => e.AnnualSalary > annualSalaryCompare);
-            if (isTrueAny)
-            {
-                Console.WriteLine($"At least one employee Annual Salary is above {annualSalaryCompare}");
-            }
-            else 
-            {
-                Console.WriteLine($"Not one employee Annual Salary is above {annualSalaryCompare}");
-            }
+            //bool isTrueAny = employeeList.Any(e => e.AnnualSalary > annualSalaryCompare);
+            //if (isTrueAny)
+            //{
+            //    Console.WriteLine($"At least one employee Annual Salary is above {annualSalaryCompare}");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine($"Not one employee Annual Salary is above {annualSalaryCompare}");
+            //}
+
+
+            //Contains Operator
+            //Returns true if we pass in an object that is equal in value with an object in the relevant collection
+            //For primitive types like int, string, or simple classes like Employee with standard equality semantics,
+            //Contains works out of the box. However, if you want to customize how equality is determined for complex
+            //types or want to use Contains with custom equality logic, you can either override Equals and GetHashCode
+            //in your class or implement IEqualityComparer and provide your own comparison logic. This allows you to
+            //define how equality is determined for instances of your class.
+            //var searchEmployee = new Employee
+            //{
+            //    Id = 3,
+            //    FirstName = "Douglas",
+            //    LastName = "Roberts",
+            //    AnnualSalary = 40000.2m,
+            //    IsManager = false,
+            //    DepartmentId = 2
+            //};
+
+            //bool containsEmployee = employeeList.Contains(searchEmployee, new EmployeeComparer());
+            //if (containsEmployee)
+            //{
+            //    Console.WriteLine($"{searchEmployee.FirstName} {searchEmployee.LastName} with ID number {searchEmployee.Id} is contained within the collection");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine($"No employee named {searchEmployee.FirstName} {searchEmployee.LastName} with ID number {searchEmployee.Id} was found within the collection");
+            //}
+
+
+
+
+
+
         }
 
 
 
 
         
+    }
+    //Contains() by default will compare objects by reference.
+    //For more complex comparisons you need to customise you comparison logic 
+    //by implementing the IEqualityComparer in your class like this and then using your own
+    //logic inside the two methods Equals() and GetHashCode{} like this:
+    public class EmployeeComparer : IEqualityComparer<Employee>
+    {
+        public bool Equals(Employee? x, Employee? y)
+        {
+            if (x.Id == y.Id && x.FirstName == y.FirstName && x.LastName == y.LastName)
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+
+        public int GetHashCode([DisallowNull] Employee obj)
+        {
+            return obj.Id.GetHashCode();
+        }
+
     }
 
     public class Employee

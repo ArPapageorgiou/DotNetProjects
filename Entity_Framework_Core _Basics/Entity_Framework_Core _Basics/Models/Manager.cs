5c843add63d350e1430550faa9fc8f591d18ff24
Argiris <ar.papagewrgiou@gmail.com>
2024-04-09 00:38:47 +0300

    Demonstrated how to add data to database with SaveChanges method

--- Entity_Framework_Core _Basics/Entity_Framework_Core _Basics/Models/Manager.cs
+++ Entity_Framework_Core _Basics/Entity_Framework_Core _Basics/Models/Manager.cs
@@ -9,7 +9,6 @@
         //Since there is a one-to-many relationship with Employee we have to use this collection navigation
         //property to represent the Employees managed by the manager. ICollection is used to to represent a
         //collection navigation property in a one-to-many relationship. In essense the bellow setup means this
-
         //manager can have more than one employees. Since the foreign key in Employee is pointing to the manager
         //class, that makes the manager class the Principal entity.
 

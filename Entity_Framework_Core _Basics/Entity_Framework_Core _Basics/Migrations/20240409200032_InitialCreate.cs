306bde17734c557626de6e6c802fd289e36184dd
ArPapageorgiou <159928305+ArPapageorgiou@users.noreply.github.com>
2024-04-09 23:23:47 +0300

    Linq (#63)
    * StoredProcedureCall_WithParameter_DeleteRows
    
    * added ADO_StoredProcedureCall_WithParameter1and2
    
    * ADO_DataSet_StoredProcedure_2_CreateCommandVersion
    
    * Return Dataset From Stored Procedures using Generic Methods
    
    * Updated StudentProject_ADO_CleanArchitecture
    
    * ADO_UsingStoredProcedure and update Student Project
    
    * Added Stored Procedure call with Input and output parameters
    
    * Dependency_Injection_Using_IServiceCollection_IHost_Revision
    
    * Fixed mistake in both Solutions
    
    * AppConfig_Revision
    
    * added ComplexConfigurationsSection mapping
    
    * Added ExtremeCoonfiguration mappings and used CustomConfigClasses in main
    
    * Implemented more StudentRepository methods
    
    * Added  GetAllStudentsWithProcedure method to StudentRepository
    
    * Added new Methods to StudentRepository
    
    * Added more Method implementations sto StudentRepository
    
    * Added new Method Implementations in StudentRepository
    
    * Added new Method Implementations in StudentRepository
    
    * Resolved dependencies for Infrastructure and created application's host in ADO.Service.Program
    
    * Added new Bulk Inser & Copy practice project
    
    * Corrected a mistake in the conditiion  of the Application class methods where the conditiion was if(_students is null || _students.Any()) as it always through an exception. Also corrected a mistake in the app.config path to the DataBase Configuration class.
    
    * Revisited Delegates for practice.
    
    * Lamda_Revision
    
    * Added new practice projects for revision purposes.
    
    * Demonstrated the use of LINQ Method Syntax and LINQ Query Syntax in Program
    
    * Demonstrated Usage of Method and LINQ Query syntex for Inner Join in Main
    
    * Added Group Join examples in Main using Method and LINQ Query syntax.
    
    * Created small Examples for OrderBy, GroupBy, ToLookUp operators. Also included examples for All and Any Quantifiers.
    
    * Created example of Contains() method with custom comparer
    
    * Created examples for more LINQ methods.
    
    * Added some comments to help with later revision
    
    * Added examples for the Equality and Concatination LINQ method
    
    * Added example of Aggregate function.
    
    * Added more Aggregate Function examples with comments for later revision.
    
    * Added more LINQ method examples to Main()
    
    * Added new LINQ method examples in Main()
    
    * Added more LINQ method examples in Main()
    
    * Added more LINQ method examples in Main()
    
    * Added more LINQ method examples in Main()
    
    * Created Entity classes and DbContext class
    
    * Added a project demonstrating Insert with SqlBulkCopy
    
    * Executed Add-Migaration command and Update-Database command  in package manager console. Database has been created.
    
    * Added new entity class EmployeeDetails with a one to one relationship to the Employee class. Added Migration and updated Database.
    
    * Established a new one to may relationship between entity classes and applied it to the database.
    
    * Defined Many-To-Many relationship between  the Employee, Project, and EmployeeProject entities by overriding OnModelCreating method. Applied migration to databse.
    
    * Made notes on demonstrating usage of Data annotations vs Fluent API for configuration purposes.
    
    * Demonstrated how to add data to database with SaveChanges method
    
    * added Migrations from scratch and started adding records to the database from Main()
    
    ---------
    
    Co-authored-by: Argiris <ar.papagewrgiou@gmail.com>

---
+++ Entity_Framework_Core _Basics/Entity_Framework_Core _Basics/Migrations/20240409200032_InitialCreate.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entity_Framework_Core__Basics.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Managers",
+                columns: table => new
+                {
+                    ManagerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MngFirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MngLastName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Managers", x => x.ManagerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Project",
+                columns: table => new
+                {
+                    ProjectId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Project", x => x.ProjectId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmpSalary = table.Column<long>(type: "bigint", nullable: false),
+                    ManagerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                    table.ForeignKey(
+                        name: "FK_Employees_Managers_ManagerId",
+                        column: x => x.ManagerId,
+                        principalTable: "Managers",
+                        principalColumn: "ManagerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmployeeDetails_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "EmployeeId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeProjects",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    ProjectId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeProjects", x => new { x.ProjectId, x.EmployeeId });
+                    table.ForeignKey(
+                        name: "FK_EmployeeProjects_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "EmployeeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EmployeeProjects_Project_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Project",
+                        principalColumn: "ProjectId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeDetails_EmployeeId",
+                table: "EmployeeDetails",
+                column: "EmployeeId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeProjects_EmployeeId",
+                table: "EmployeeProjects",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_ManagerId",
+                table: "Employees",
+                column: "ManagerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeDetails");
+
+            migrationBuilder.DropTable(
+                name: "EmployeeProjects");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Project");
+
+            migrationBuilder.DropTable(
+                name: "Managers");
+        }
+    }
+}
--- Entity_Framework_Core _Basics/Entity_Framework_Core _Basics/Program.cs
+++ Entity_Framework_Core _Basics/Entity_Framework_Core _Basics/Program.cs
@@ -1,41 +1,117 @@
-﻿using Entity_Framework_Core__Basics.Data;
-using Entity_Framework_Core__Basics.Models;
-//HOW TO ADD DATA TO DATABASE
 
-//1)Create instances of your model classes 
+﻿using Entity_Framework_Core__Basics.Models;
+using Entity_Framework_Core__Basics.Data;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
-//2)Add them to the appropriate DBSet property in your DBContext class
-
-//Call the SaveChanges method
-
-
-//to add data to the database we need to create an instance of AppDBContext so  that we can access it's
-//properties. We do this with a using statement to make sure that the instance is properly disposed of 
-//afterwards.
-using (var context = new AppDBContext()) 
+namespace Entity_Framework_Core__Basics
 {
-    //Insert manager details
-    //var manager = new Manager();
-    //manager.MngFirstName = "Ravi";
-    //manager.MngLastName = "G";
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
-    //add data to the DbSet
-    //context.Managers.Add(manager);
+            //HOW TO ADD DATA TO DATABASE
 
-    //The SaveChanges method passes data to the database
-    /*context.SaveChanges();*/
+            //1)Create instances of your model classes 
 
-    //Insert Employee details
-    var employee = new Employee();
-    employee.FirstName = "Argiris";
-    employee.LastName = "Papageorgiou";
-    employee.EmpSalary = 100000;
-    employee.ManagerId = 1;
+            //2)Add them to the appropriate DBSet property in your DBContext class
 
-    context.Employees.Add(employee);
-
-    context.SaveChanges();
+            //Call the SaveChanges method
 
 
+            //to add data to the database we need to create an instance of AppDBContext so  that we can access it's
+            //properties. We do this with a using statement to make sure that the instance is properly disposed of 
+            //afterwards.
+            try 
+            { 
+            using (var context = new AppDBContext())
+            {
+                    try 
+                    { 
+                    using (var transaction = context.Database.BeginTransaction())
+                    {
+                            //Insert manager details
+                            var manager = new Manager();
+                            manager.MngFirstName = "Ravi";
+                            manager.MngLastName = "G";
+                            //add data to the DbSet
+                            //context.Managers.Add(manager);
+
+                            var manager2 = new Manager();
+                            manager2.MngFirstName = "Sam";
+                            manager2.MngLastName = "K";
+                            //context.Managers.Add(manager2);
+
+                            var employee = new Employee();
+                            employee.FirstName = "Argiris";
+                            employee.LastName = "Papageorgiou";
+                            employee.EmpSalary = 100000;
+                            employee.ManagerId = 1;
+                            //context.Employees.Add(employee);
+
+                            var employee2 = new Employee();
+                            employee2.FirstName = "Giorgos";
+                            employee2.LastName = "Papageorgiou";
+                            employee2.EmpSalary = 120000;
+                            employee2.ManagerId = 1;
+                            //context.Employees.Add(employee2);
+
+                            var project = new Project();
+                            project.Name = "Development";
+                            //context.Project.Add(project);
+
+                            //var employeeProject = new EmployeeProject();
+                            //employeeProject.Project = project;
+                            //employeeProject.Employee = employee2;
+                            //context.EmployeeProjects.Add(employeeProject); 
+
+                            var employeeDetails = new EmployeeDetails();
+                            employeeDetails.Address = "Sarakou";
+                            employeeDetails.PhoneNumber = "12345098798";
+                            employeeDetails.Email = "papagiorgio@ado.net";
+                            employeeDetails.EmployeeId = employee.EmployeeId;
+                            context.EmployeeDetails.Add(employeeDetails);
+
+                            var employeeDetails2 = new EmployeeDetails();
+                            employeeDetails2.Address = "Gripari";
+                            employeeDetails2.PhoneNumber = "4563798798";
+                            employeeDetails2.Email = "giorgio90@ado.net";
+                            employeeDetails2.EmployeeId = employee2.EmployeeId;
+                            context.EmployeeDetails.Add(employeeDetails2);
+
+                            //The SaveChanges method passes data to the database
+                            context.SaveChanges();
+                        //If all operations succeed then commit the transaction
+                        transaction.Commit();
+                    }
+                    }
+                    catch (Exception ex) 
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+
+
+
+            }
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 

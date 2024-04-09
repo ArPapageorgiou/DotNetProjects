45fae5d0ac0c8861b71a5e4093016a507e7a8e09
ArPapageorgiou <159928305+ArPapageorgiou@users.noreply.github.com>
2024-04-10 02:43:11 +0300

    Linq (#65)
    * StoredProcedureCall_WithParameter_DeleteRows
    
    * added ADO_StoredProcedureCall_WithParameter1and2
    
    * ADO_DataSet_StoredProcedure_2_CreateCommandVersion
    
    * Return Dataset From Stored Procedures using Generic Methods
    
    * Updated StudentProject_ADO_CleanArchitecture
    
    * ADO_UsingStoredProcedure and update Student Project
    
    * Added Stored Procedure call with Input and output parameters
    
    * Dependency_Injection_Using_IServiceCollection_IHost_Revision
    
    * Fixed mistake in both Solutions
    
    * AppConfig_Revision
    
    * added ComplexConfigurationsSection mapping
    
    * Added ExtremeCoonfiguration mappings and used CustomConfigClasses in main
    
    * Implemented more StudentRepository methods
    
    * Added  GetAllStudentsWithProcedure method to StudentRepository
    
    * Added new Methods to StudentRepository
    
    * Added more Method implementations sto StudentRepository
    
    * Added new Method Implementations in StudentRepository
    
    * Added new Method Implementations in StudentRepository
    
    * Resolved dependencies for Infrastructure and created application's host in ADO.Service.Program
    
    * Added new Bulk Inser & Copy practice project
    
    * Corrected a mistake in the conditiion  of the Application class methods where the conditiion was if(_students is null || _students.Any()) as it always through an exception. Also corrected a mistake in the app.config path to the DataBase Configuration class.
    
    * Revisited Delegates for practice.
    
    * Lamda_Revision
    
    * Added new practice projects for revision purposes.
    
    * Demonstrated the use of LINQ Method Syntax and LINQ Query Syntax in Program
    
    * Demonstrated Usage of Method and LINQ Query syntex for Inner Join in Main
    
    * Added Group Join examples in Main using Method and LINQ Query syntax.
    
    * Created small Examples for OrderBy, GroupBy, ToLookUp operators. Also included examples for All and Any Quantifiers.
    
    * Created example of Contains() method with custom comparer
    
    * Created examples for more LINQ methods.
    
    * Added some comments to help with later revision
    
    * Added examples for the Equality and Concatination LINQ method
    
    * Added example of Aggregate function.
    
    * Added more Aggregate Function examples with comments for later revision.
    
    * Added more LINQ method examples to Main()
    
    * Added new LINQ method examples in Main()
    
    * Added more LINQ method examples in Main()
    
    * Added more LINQ method examples in Main()
    
    * Added more LINQ method examples in Main()
    
    * Created Entity classes and DbContext class
    
    * Added a project demonstrating Insert with SqlBulkCopy
    
    * Executed Add-Migaration command and Update-Database command  in package manager console. Database has been created.
    
    * Added new entity class EmployeeDetails with a one to one relationship to the Employee class. Added Migration and updated Database.
    
    * Established a new one to may relationship between entity classes and applied it to the database.
    
    * Defined Many-To-Many relationship between  the Employee, Project, and EmployeeProject entities by overriding OnModelCreating method. Applied migration to databse.
    
    * Made notes on demonstrating usage of Data annotations vs Fluent API for configuration purposes.
    
    * Demonstrated how to add data to database with SaveChanges method
    
    * added Migrations from scratch and started adding records to the database from Main()
    
    * Added records.
    
    * Demonstrated how to Retrieve, Update and Delete data using LINQ methods
    
    ---------
    
    Co-authored-by: Argiris <ar.papagewrgiou@gmail.com>

--- Entity_Framework_Core _Basics/Entity_Framework_Core _Basics/Program.cs
+++ Entity_Framework_Core _Basics/Entity_Framework_Core _Basics/Program.cs
@@ -22,89 +22,175 @@ namespace Entity_Framework_Core__Basics
             //to add data to the database we need to create an instance of AppDBContext so  that we can access it's
             //properties. We do this with a using statement to make sure that the instance is properly disposed of 
             //afterwards.
-            try 
-            { 
-            using (var context = new AppDBContext())
-            {
-                    try 
-                    { 
-                    using (var transaction = context.Database.BeginTransaction())
-                    {
-                            //Insert manager details
-                            var manager = new Manager();
-                            manager.MngFirstName = "Ravi";
-                            manager.MngLastName = "G";
-                            //add data to the DbSet
-                            //context.Managers.Add(manager);
 
-                            var manager2 = new Manager();
-                            manager2.MngFirstName = "Sam";
-                            manager2.MngLastName = "K";
-                            //context.Managers.Add(manager2);
+            //try 
+            //{ 
+            //using (var context = new AppDBContext())
+            //{
+            //        try 
+            //        { 
+            //        using (var transaction = context.Database.BeginTransaction())
+            //        {
+            //                //Insert manager details
+            //                var manager = new Manager();
+            //                manager.MngFirstName = "Ravi";
+            //                manager.MngLastName = "G";
+            //                //add data to the DbSet
+            //                //context.Managers.Add(manager);
 
-                            var employee = new Employee();
-                            employee.FirstName = "Argiris";
-                            employee.LastName = "Papageorgiou";
-                            employee.EmpSalary = 100000;
-                            employee.ManagerId = 1;
-                            //context.Employees.Add(employee);
+            //                var manager2 = new Manager();
+            //                manager2.MngFirstName = "Sam";
+            //                manager2.MngLastName = "K";
+            //                //context.Managers.Add(manager2);
 
-                            var employee2 = new Employee();
-                            employee2.FirstName = "Giorgos";
-                            employee2.LastName = "Papageorgiou";
-                            employee2.EmpSalary = 120000;
-                            employee2.ManagerId = 1;
-                            //context.Employees.Add(employee2);
+            //                var employee = new Employee();
+            //                employee.FirstName = "Argiris";
+            //                employee.LastName = "Papageorgiou";
+            //                employee.EmpSalary = 100000;
+            //                employee.ManagerId = 1;
+            //                //context.Employees.Add(employee);
 
-                            var project = new Project();
-                            project.Name = "Development";
-                            //context.Project.Add(project);
+            //                var employee2 = new Employee();
+            //                employee2.FirstName = "Giorgos";
+            //                employee2.LastName = "Papageorgiou";
+            //                employee2.EmpSalary = 120000;
+            //                employee2.ManagerId = 1;
+            //                //context.Employees.Add(employee2);
 
-                            var employeeProject = new EmployeeProject();
-                            employeeProject.ProjectId = 1;
-                            employeeProject.EmployeeId = 2;
-                            //context.EmployeeProjects.Add(employeeProject);
+            //                var project = new Project();
+            //                project.Name = "Development";
+            //                //context.Project.Add(project);
+
+            //                var employeeProject = new EmployeeProject();
+            //                employeeProject.ProjectId = 1;
+            //                employeeProject.EmployeeId = 2;
+            //                //context.EmployeeProjects.Add(employeeProject);
+
+            //                var employeeDetails = new EmployeeDetails();
+            //                employeeDetails.Address = "Sarakou";
+            //                employeeDetails.PhoneNumber = "12345098798";
+            //                employeeDetails.Email = "papagiorgio@ado.net";
+            //                employeeDetails.EmployeeId = 1;
+            //                context.EmployeeDetails.Add(employeeDetails);
+
+            //                var employeeDetails2 = new EmployeeDetails();
+            //                employeeDetails2.Address = "Gripari";
+            //                employeeDetails2.PhoneNumber = "4563798798";
+            //                employeeDetails2.Email = "giorgio90@ado.net";
+            //                employeeDetails2.EmployeeId = 2;
+            //                context.EmployeeDetails.Add(employeeDetails2);
 
 
-                            var employeeDetails = new EmployeeDetails();
-                            employeeDetails.Address = "Sarakou";
-                            employeeDetails.PhoneNumber = "12345098798";
-                            employeeDetails.Email = "papagiorgio@ado.net";
-
-                            employeeDetails.EmployeeId = 1;
-
-                            context.EmployeeDetails.Add(employeeDetails);
-
-                            var employeeDetails2 = new EmployeeDetails();
-                            employeeDetails2.Address = "Gripari";
-                            employeeDetails2.PhoneNumber = "4563798798";
-                            employeeDetails2.Email = "giorgio90@ado.net";
-
-                            employeeDetails2.EmployeeId = 2;
-
-                            context.EmployeeDetails.Add(employeeDetails2);
-
-                            //The SaveChanges method passes data to the database
-                            context.SaveChanges();
-
-                            //If all operations succeed then commit the transaction
-                            transaction.Commit();
-
-                    }
-                    }
-                    catch (Exception ex) 
-                    {
-                        Console.WriteLine(ex.Message);
-                    }
+            //                //The SaveChanges method passes data to the database
+            //                context.SaveChanges();
+            //                //If all operations succeed then commit the transaction
+            //                transaction.Commit();
+            //        }
+            //        }
+            //        catch (Exception ex) 
+            //        {
+            //            Console.WriteLine(ex.Message);
+            //        }
 
 
 
-            }
-            }
-            catch (Exception ex) 
-            {
-                Console.WriteLine($"Error: {ex.Message}");
-            }
+            //}
+            //}
+            //catch (Exception ex) 
+            //{
+            //    Console.WriteLine($"Error: {ex.Message}");
+            //}
+
+            /////////Retrieve, Update and Delete data in ef core using LINQ
+
+            //Let's try to retrieve data from the database
+            //try
+            //{
+            //    using (var context = new AppDBContext()) 
+            //    { 
+            //    var employees = context.Employees.ToList();
+
+            //        foreach (var employee in employees) 
+            //        {
+            //            Console.WriteLine($"{employee.FirstName} {employee.LastName} {employee.EmployeeId}");
+            //        }
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+
+            //    Console.WriteLine("Error: " + e);
+            //}
+
+            //RETRIEVE
+            //Now let's try to retrieve a specific set of data from the database
+            //try
+            //{
+            //    using (var context = new AppDBContext()) 
+            //    {
+            //        var employee = context.Employees.Single(e => e.EmployeeId == 1);
+            //        Console.WriteLine($"{employee.FirstName} {employee.LastName} {employee.EmployeeId}");
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+
+            //    Console.WriteLine("Error: " + e); ;
+            //}
+
+            //UPDATE
+            //!. Retrieve the entity
+            //2. Update the necessary properties of retrieved entity
+            //3. Save the changes by calling SaveChanges() method
+
+            //try
+            //{
+            //    using (var context = new AppDBContext()) 
+            //    {
+            //        var employee = context.Employees.Single(e => e.EmployeeId == 2);
+            //        employee.EmpSalary = 110000;
+            //        context.SaveChanges(); //until we call SaveChanges() method nothing will be updated in the datbase
+
+            //        //Retrieve employee just to verify
+            //        Console.WriteLine($"{employee.FirstName} {employee.LastName} {employee.EmployeeId} {employee.EmpSalary}");
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+
+            //    Console.WriteLine("Error: " + e); 
+            //}
+
+            //DELETE
+            //!. Retrieve the entity
+            //2. Delete the entity using the Remove() method
+            //3. Save the changes by calling SaveChanges() method
+
+            //try
+            //{
+            //    using (var context = new AppDBContext()) 
+            //    {
+            //        var employee = context.Employees.Single(e => e.EmployeeId == 1);
+            //        if (employee != null)
+            //        {
+            //            context.Employees.Remove(employee);
+            //            context.SaveChanges();
+            //            Console.WriteLine($"{employee.FirstName} {employee.LastName} {employee.EmployeeId} {employee.EmpSalary}");
+            //        }
+            //        else 
+            //        { 
+            //            Console.WriteLine($"Record no longer exists in Database");
+            //        }
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+
+            //    Console.WriteLine("Error: " + e);
+            //}
+
+
+
 
         }
     }

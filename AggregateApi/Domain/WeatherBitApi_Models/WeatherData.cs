a6c6aab6a8535f412484c40cdf1db0dac0776850
ArPapageorgiou <159928305+ArPapageorgiou@users.noreply.github.com>
2024-07-18 00:25:35 +0300

    The project fetches weather data from WeatherBit API, processes it, and exposes it as a consumable service through a controller endpoint. It includes a fallback mechanism and circuit-breaker, redis cache for performance optimization as well as a service that collects statistics concerning requests made to the API.  This is intended to include more APIs and  to create an aggregate service to be exposed through a single endpoint.

---
+++ AggregateApi/Domain/WeatherBitApi_Models/WeatherData.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Domain.WeatherBitApi_Models
+{
+    public class WeatherData
+    {
+        [JsonPropertyName("data")]
+        public List<Data> DataList { get; set; }
+
+        [JsonPropertyName("count")]
+        public int Count { get; set; }
+    }
+}
---
+++ AggregateApi/Infrastructure/Repositories/RequestStatisticRepository.cs
@@ -0,0 +1,21 @@
+﻿using Application.Interfaces;
+using Domain.ApiRequestStatistic;
+using System.Collections.Concurrent;
+
+namespace Infrastructure.Repositories
+{
+    internal class RequestStatisticRepository : IRequestStatisticRepository
+    {
+        private readonly ConcurrentBag<RequestStatistic> _requestStatistics = new ConcurrentBag<Domain.ApiRequestStatistic.RequestStatistic>();
+        
+        public void AddRequestStatistics(RequestStatistic requestStatistic)
+        {
+            _requestStatistics.Add(requestStatistic);
+        }
+
+        public IEnumerable<RequestStatistic> GetStatistic()
+        {
+            return _requestStatistics;
+        }
+    }
+}

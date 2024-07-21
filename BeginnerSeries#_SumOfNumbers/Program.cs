8fdeaf31bdd6f986d1dd18f59e67e90eb48e75cc
ArPapageorgiou <159928305+ArPapageorgiou@users.noreply.github.com>
2024-07-21 15:15:18 +0300

    Feature kyu 7 kata (#120)
    * Added CountingSheep console app with traditional Main method
    
    * Function that counts the booleans set to true and rreturns an integer.
    
    * Created a function to find the sum of all integers between two given numbers, including them.

---
+++ BeginnerSeries#_SumOfNumbers/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace BeginnerSeries3_SumOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Given two integers a and b, which can be positive or negative, find the sum 
+            of all the integers between and including them and return it. If the two numbers 
+            are equal return a or b.
+
+            Note: a and b are not ordered!
+
+            Examples (a, b) --> output (explanation)
+
+            (1, 0) --> 1 (1 + 0 = 1)
+            (1, 2) --> 3 (1 + 2 = 3)
+            (0, 1) --> 1 (0 + 1 = 1)
+            (1, 1) --> 1 (1 since both are same)
+            (-1, 0) --> -1 (-1 + 0 = -1)
+            (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)*/
+
+            int a = -7;
+            int b = 2;
+
+            Console.WriteLine(GetSum(a,b));
+
+        }
+        
+        public static int GetSum(int a, int b)
+     {
+       
+       int sum = 0;
+       
+       if(a < b)
+       {
+        
+         while(a<=b)
+         {
+           sum += a;
+           a++;
+         }
+         
+         return sum;
+         
+       }
+       
+       else if(b<a)
+       {
+         
+         while(b<=a)
+         {
+           sum += b;
+           b++;
+         }
+         
+         return sum;
+         
+       }
+       
+       else
+       {
+         return a;
+       }
+     }
+
+    }
+}

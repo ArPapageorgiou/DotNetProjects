1bc562553978bdf094432b3a79482acc15a77d13
ArPapageorgiou <ar.papageorgiou@gmail.com>
2024-07-21 12:53:48 +0300

    Function that counts the booleans set to true and rreturns an integer.

--- CountingSheep/Program.cs
+++ CountingSheep/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CountingSheep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Consider an array/list of sheep where some sheep may be missing from their 
+            place. We need a function that counts the number of sheep present in the 
+            array (true means present).
+
+            For example,
+
+            [true,  true,  true,  false,
+            true,  true,  true,  true ,
+            true,  false, true,  false,
+            true,  false, false, true ,
+            true,  true,  true,  true ,
+            false, false, true,  true]
+            The correct answer would be 17.
+
+            Hint: Don't forget to check for bad values like null/undefined*/
+
+            bool[] flock = { true, false, true, false,};
+
+            Console.WriteLine(CountSheeps(flock));
+
+
+        }
+
+        public static int CountSheeps(bool[] sheeps)
+        {
+            int count = 0;
+
+            foreach (bool sheepsItem in sheeps) 
+            {
+                if (sheepsItem == true)
+                {
+                    count++;
+                }
+
+            }
+
+            return count;
+
+        }
+
+    }
+}
+

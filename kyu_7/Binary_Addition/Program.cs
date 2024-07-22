f61fa076484f620f533e6d26444540b733a7a585
ArPapageorgiou <159928305+ArPapageorgiou@users.noreply.github.com>
2024-07-22 11:04:07 +0300

    Feature kyu 7 kata (#122)
    * Added CountingSheep console app with traditional Main method
    
    * Function that counts the booleans set to true and rreturns an integer.
    
    * Created a function to find the sum of all integers between two given numbers, including them.
    
    * added a more concise implementation of the function.
    
    * Created function that adds two numbers together and returns their sum in binary. Also added a much shorter implementation.
    
    * organized projects in the same file.
    
    * new commit

---
+++ kyu_7/Binary_Addition/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace Ninary_Addition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Implement a function that adds two numbers together and returns 
+            their sum in binary. The conversion can be done before, or after 
+            the addition.
+
+            The binary number returned should be a string.
+
+            Examples:(Input1, Input2 --> Output (explanation)))
+
+            1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+            5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)*/
+            
+            int a = 5;
+            int b = 9;
+
+            Console.WriteLine(AddBinary(a,b));
+
+        }
+        
+        /*public static string AddBinary(int a, int b)
+        {
+            int sum = a + b;
+            string binarySum = "";
+
+            if(sum == 0) return "0";
+
+            while(sum > 0)
+            {
+                int remainder = sum % 2;
+                binarySum  = remainder.ToString() + binarySum;
+                sum /= 2; 
+            }
+
+            return binarySum;
+
+        }*/
+
+        //This is a much shorter way to do this. Quite handy.
+        static static string AddBinary(int a, int b)
+        {
+            Convert.ToString(a+b, 2);
+        }
+
+    }
+}

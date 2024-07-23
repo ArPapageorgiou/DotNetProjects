9b32c3e3dd4c4fa5b39e1b3a99d21c3ccd05fd47
ArPapageorgiou <159928305+ArPapageorgiou@users.noreply.github.com>
2024-07-23 13:40:33 +0300

    Bubble sort generics assessment (#125)
    * Added CountingSheep console app with traditional Main method
    
    * Function that counts the booleans set to true and rreturns an integer.
    
    * Created a function to find the sum of all integers between two given numbers, including them.
    
    * added a more concise implementation of the function.
    
    * Created function that adds two numbers together and returns their sum in binary. Also added a much shorter implementation.
    
    * organized projects in the same file.
    
    * new commit
    
    * Added a project with a pin validation function that checks if a pin has the correct format.
    
    * Created Bubble class and added Print, Swap and Sorting functions. Also Created a custom Exception class NullArrayException.
    
    * Called the Methods from Bubble.cs with the arrays and in the order specified in the assesment documentation.

--- BubbleSort/BubbleSort_Generics_Project/Program.cs
+++ BubbleSort/BubbleSort_Generics_Project/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Generics_BubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Initialize arrays to be sorted
+                int[] numbersToSort = { 5, 4, 2, 1, 3 };
+                char[] charactersTosort = { 'a', 'd', 'c', 'b', 'e' };
+
+                // Create Bubble sort objects for integers and characters
+                Bubble<int> numbers = new Bubble<int>(numbersToSort);
+                Bubble<char> characters = new Bubble<char>(charactersTosort);
+
+                // Perform sorting on the integer array
+                numbers.Print();
+                numbers.BubbleSortAscending();
+                numbers.Print();
+                numbers.BubbleSortDescending();
+                numbers.Print();
+
+                // Perform sorting on the character array
+                characters.Print();
+                characters.BubbleSortAscending();
+                characters.Print();
+                characters.BubbleSortDescending();
+                characters.Print();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred. {ex.Message}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+

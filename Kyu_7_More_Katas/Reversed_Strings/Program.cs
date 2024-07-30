3019ff1546d850d70c2b57285196d8abc88939d5
Argiris Papageorgiou <ar.papageorgiou@gmail.com>
2024-07-30 13:40:22 +0300

    Added project on how to reverse strings.

---
+++ Kyu_7_More_Katas/Reversed_Strings/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Reversed_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Complete the solution so that it reverses the string passed into it.
+
+            'world'  =>  'dlrow'
+            'word'   =>  'drow'*/
+
+            string someString = "Hello world";
+            Console.WriteLine(ReverseString(someString));
+
+        }
+
+        /*public static string ReverseString(string str)
+        {
+            str = str.Trim();
+
+            char[] strArray = str.ToCharArray();
+
+            string result = "";
+
+            foreach (char c in strArray)
+            {
+                result = c + result;
+            }
+
+            return result;
+        }*/
+
+
+        //A much more concise way of doing this.
+        public static string ReverseString(string str)
+        {
+            char[] strArray = str.ToCharArray();
+            Array.Reverse(strArray);
+
+            return new string(strArray);
+        }
+
+    }
+}

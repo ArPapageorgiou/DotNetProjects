be8c6a072a4a2f0b0b0778d6f777541685db13b9
Argiris Papageorgiou <ar.papageorgiou@gmail.com>
2024-08-01 17:20:53 +0300

    Created a function that removes the first and last characters of a string. The function has two versions, one using Remove() and then one using Substring() as it is more concise.

---
+++ CodeWars_Challenges/Kyu_7_More_Katas/Remove_First_and_Last_Character/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Remove_First_and_Last_Character
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Your goal is to create a function that removes the first and last characters of a string. 
+             * You're given one parameter, the original string. You don't have to worry about strings with less than two characters.*/
+
+            string wordOrPhrase = "Hello world!";
+            Console.WriteLine(Remove_char(wordOrPhrase));
+
+        }
+
+        /*public static string Remove_char(string str)
+        {
+            str = str.Remove(0,1);
+            str = str.Remove(str.Length -1, 1);
+
+            return str;
+        }*/
+
+        //Substring(startIndex, length) is another, more concise way of removing the first and last char of the string.
+        public static string Remove_char(string str)
+        {
+            return str.Substring(1, str.Length - 2);
+        }
+
+    }
+}

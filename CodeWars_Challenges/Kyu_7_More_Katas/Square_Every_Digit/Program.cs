5d8710195494b1e6d914a1e7a2224aaf752d707c
ArPapageorgiou <159928305+ArPapageorgiou@users.noreply.github.com>
2024-08-03 22:53:55 +0300

    Codewars challenges (#140)
    * reorganized my repo
    
    * Created a function that removes the first and last characters of a string. The function has two versions, one using Remove() and then one using Substring() as it is more concise.
    
    * Created a function that takes an int and squares every digit before returning the concatenated result.

---
+++ CodeWars_Challenges/Kyu_7_More_Katas/Square_Every_Digit/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace Square_Every_Digit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+            For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)*/
+
+            int n = 9119;
+
+            Console.WriteLine(SquareEveryDigit(n));
+
+        }
+
+        public static int SquareEveryDigit(int n)
+        {
+            var sb = new StringBuilder();
+
+            foreach (char c in n.ToString())
+            {
+                int digit = int.Parse(c.ToString());
+                sb.Append(digit * digit);
+            }
+
+            return int.Parse(sb.ToString());
+        }
+
+    }
+}

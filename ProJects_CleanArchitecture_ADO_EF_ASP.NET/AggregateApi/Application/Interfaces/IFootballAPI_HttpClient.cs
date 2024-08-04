d5b04b62ead19040bb63d5c52c8fc57e9241b42c
ArPapageorgiou <159928305+ArPapageorgiou@users.noreply.github.com>
2024-08-04 21:17:44 +0300

    Configured FootballAPIStanding in  appsettings and created FootballAPI_HttpClient in Infrastructure layer. (#141)

---
+++ ProJects_CleanArchitecture_ADO_EF_ASP.NET/AggregateApi/Application/Interfaces/IFootballAPI_HttpClient.cs
@@ -0,0 +1,9 @@
+﻿using Domain.FootballAPI_ModelClasses.ApiFootball;
+
+namespace Application.Interfaces
+{
+    public interface IFootballAPI_HttpClient
+    {
+        Task<ApiResponse> GetFootballData(string leagueId, string season);
+    }
+}
---
+++ ProJects_CleanArchitecture_ADO_EF_ASP.NET/AggregateApi/Application/Interfaces/IFootballStandingsService.cs
@@ -0,0 +1,10 @@
+﻿using Domain.FootballAPI_ModelClasses;
+using Domain.FootballAPI_ModelClasses.ApiFootball;
+
+namespace Application.Interfaces
+{
+    internal interface IFootballStandingsService
+    {
+        Task<IEnumerable<ApiResponse>> GetFootbalStandings(string league, string season);
+    }
+}

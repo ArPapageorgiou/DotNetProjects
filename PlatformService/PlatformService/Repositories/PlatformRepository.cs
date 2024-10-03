4f74fe6b018d9faddebf96db5a69f93adf995607
ArPapageorgiou <159928305+ArPapageorgiou@users.noreply.github.com>
2024-10-03 23:52:26 +0300

    Added PlatformRepository.cs and implemented all IPlatformRepository methods. (#180)

---
+++ PlatformService/PlatformService/Repositories/PlatformRepository.cs
@@ -0,0 +1,43 @@
+﻿using PlatformService.Data;
+using PlatformService.Interfaces;
+using PlatformService.Models;
+
+namespace PlatformService.Repositories
+{
+    public class PlatformRepository : IPlatformRepository
+    {
+        
+        private readonly AppDbContext _appDbContext;
+
+        public PlatformRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public void CreatePlatform(Platform platform)
+        {
+            if(platform == null)
+            {
+                throw new ArgumentNullException(nameof(platform));
+            }
+
+            _appDbContext.Add(platform);
+            _appDbContext.SaveChanges();
+        }
+
+        public Platform GetPlatformById(int Id)
+        {
+            return _appDbContext.Platforms.FirstOrDefault(p => p.Id == Id);
+        }
+
+        public IEnumerable<Platform> GetPlatforms()
+        {
+            return _appDbContext.Platforms.ToList();
+        }
+
+        public bool SaveChanges()
+        {
+            return (_appDbContext.SaveChanges() >= 0);
+        }
+    }
+}

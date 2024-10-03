e25c43a51f80746b879aee6744950ca95f1d68b4
Argiris Papageorgiou <ar.papageorgiou@gmail.com>
2024-10-03 20:07:27 +0300

    Created AppDbContext class and registered in Program.cs

--- PlatformService/PlatformService/Program.cs
+++ PlatformService/PlatformService/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
 namespace PlatformService
 {
     public class Program
@@ -8,7 +11,8 @@ namespace PlatformService
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            builder.Services.AddDbContext<AppDbContext>(opt => 
+            opt.UseInMemoryDatabase("InMemoryDb"));
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();

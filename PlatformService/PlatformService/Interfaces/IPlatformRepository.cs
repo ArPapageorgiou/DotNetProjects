c4713fbc67fbf9299225cf58cd442352f3f3fcc6
Argiris Papageorgiou <ar.papageorgiou@gmail.com>
2024-10-03 23:52:06 +0300

    Added PlatformRepository.cs and implemented all IPlatformRepository methods.

---
+++ PlatformService/PlatformService/Interfaces/IPlatformRepository.cs
@@ -0,0 +1,15 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Interfaces
+{
+    public interface IPlatformRepository
+    {
+        bool SaveChanges();
+
+        IEnumerable<Platform> GetPlatforms();
+
+        Platform GetPlatformById(int Id);
+
+        void CreatePlatform(Platform platform);
+    }
+}

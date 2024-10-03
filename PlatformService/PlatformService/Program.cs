968bdd87c016c7823ee75b483503dfdab9795b34
ArPapageorgiou <159928305+ArPapageorgiou@users.noreply.github.com>
2024-10-03 20:08:09 +0300

    Platform service models (#179)
    * Added Dependencies
    
    * Added platform model class
    
    * Created AppDbContext class and registered in Program.cs

--- PlatformService/PlatformService/Program.cs
+++ PlatformService/PlatformService/Program.cs
@@ -1,4 +1,9 @@
 
+
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
+
 namespace PlatformService
 {
     public class Program
@@ -8,6 +13,9 @@ namespace PlatformService
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            builder.Services.AddDbContext<AppDbContext>(opt => 
+            opt.UseInMemoryDatabase("InMemoryDb"));
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

b6677ca166655ebe41d1427604ce2839a1210e68
Argiris Papageorgiou <ar.papageorgiou@gmail.com>
2024-10-19 19:56:11 +0300

    Move request statistics logging to middleware
    - Centralized request metrics tracking in PerformanceMetricsMiddleware.
    - Removed redundant tracking logic from WeatherHttpClient, NewsAPI_HttpClient and FootballAPI_HttpClient.

--- ProJects_CleanArchitecture_ADO_EF_ASP.NET/AggregateApi/Infrastructure/Repositories/FootballAPI_HttpClient.cs
+++ ProJects_CleanArchitecture_ADO_EF_ASP.NET/AggregateApi/Infrastructure/Repositories/FootballAPI_HttpClient.cs
@@ -45,7 +45,7 @@ namespace Infrastructure.Repositories
 
             HttpResponseMessage response;
 
-            var stopWatch = Stopwatch.StartNew();
+            //var stopWatch = Stopwatch.StartNew();
 
             try
             {
@@ -65,17 +65,17 @@ namespace Infrastructure.Repositories
                 _logger.LogError($"Exception: {ex.Message}");
                 throw new Exception("An error occured while fetching data from the api");
             }
-            finally
-            {
-                stopWatch.Stop();
-                _requestStatisticRepository.AddRequestStatistics(new RequestStatistic
-                {
-                    ApiName = "FootBallAPI",
-                    ResponseTime = stopWatch.ElapsedMilliseconds,
-                    Timestamp = DateTime.Now
+            //finally
+            //{
+            //    stopWatch.Stop();
+            //    _requestStatisticRepository.AddRequestStatistics(new RequestStatistic
+            //    {
+            //        ApiName = "FootBallAPI",
+            //        ResponseTime = stopWatch.ElapsedMilliseconds,
+            //        Timestamp = DateTime.Now
 
-                });
-            }
+            //    });
+            //}
 
             if (response.IsSuccessStatusCode)
             {

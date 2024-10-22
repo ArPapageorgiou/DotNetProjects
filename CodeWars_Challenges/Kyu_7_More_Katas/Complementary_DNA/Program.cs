742f807a79aaf2f9c7165799479d3e4d4e081897
ArPapageorgiou <159928305+ArPapageorgiou@users.noreply.github.com>
2024-10-22 20:29:06 +0300

    Kyu 7 kata Complementary_DNA stringbuilder usage (#184)

--- CodeWars_Challenges/Kyu_7_More_Katas/Complementary_DNA/Program.cs
+++ CodeWars_Challenges/Kyu_7_More_Katas/Complementary_DNA/Program.cs
@@ -1,15 +1,52 @@
-﻿namespace Complementary_DNA
+﻿using System.Text;
+
+namespace Complementary_DNA
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            /*In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+            /*In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". 
+             * Your function receives one side of the DNA (string, except for Haskell); 
+             * you need to return the other complementary side. 
+             * DNA strand is never empty or there is no DNA at all (again, except for Haskell).
 
             Example: (input --> output)
 
             "ATTGC" --> "TAACG"
             "GTAT" --> "CATA"*/
+
+            Console.WriteLine(DnaStrings("ATGC"));
+
+        }
+
+        public static string DnaStrings(string dnaHalf)
+        {
+            StringBuilder otherHalf = new StringBuilder();
+            
+            foreach (char c in dnaHalf)
+            {
+                switch (c)
+                {
+                    case 'A':
+                        otherHalf.Append('T');
+                        break;
+
+                    case 'T':
+                        otherHalf.Append('A');
+                        break;
+
+                    case 'G':
+                        otherHalf.Append('C');
+                        break;
+
+                    case 'C':
+                        otherHalf.Append('G');
+                        break;
+                }
+            }
+
+            return otherHalf.ToString().ToUpper();
         }
     }
 }

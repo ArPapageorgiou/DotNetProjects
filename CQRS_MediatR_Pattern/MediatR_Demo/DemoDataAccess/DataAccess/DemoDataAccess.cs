24976569c0b82baf6e8d1206241a714676c21e75
Argiris Papageorgiou <ar.papageorgiou@gmail.com>
2024-11-27 17:23:35 +0200

    Setup of MediatR pattern with in-memory storage to implement CQRS

---
+++ CQRS_MediatR_Pattern/MediatR_Demo/DemoDataAccess/DataAccess/DemoDataAccess.cs
@@ -0,0 +1,27 @@
+﻿using DemoLibrary.Models;
+
+namespace DemoLibrary.DataAccess
+{
+    public class DemoDataAccess : IDataAccess
+    {
+        private readonly List<PersonModel> people = new();
+
+        public DemoDataAccess()
+        {
+            people.Add(new PersonModel { Id = 1, FirstName = "Argiris", LastName = "Papageorgiou" });
+            people.Add(new PersonModel { Id = 2, FirstName = "Tom", LastName = "Waits" });
+        }
+
+        public List<PersonModel> GetPeople()
+        {
+            return people;
+        }
+
+        public PersonModel InsertPerson(string firstName, string lastName)
+        {
+            PersonModel person = new PersonModel { Id = people.Max(x => x.Id) + 1, FirstName = firstName, LastName = lastName };
+            people.Add(person);
+            return person; //return person just to check on it's id
+        }
+    }
+}

b54faf7dda5ad0987353b944774db7ccc6c95113
Argiris Papageorgiou <ar.papageorgiou@gmail.com>
2025-01-12 11:35:37 +0200

    add

---
+++ CQRS_MediatR_Pattern/MediatR_Demo/DemoDataAccess/Commands/InsertPersonCommand.cs
@@ -0,0 +1,23 @@
+﻿using DemoLibrary.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.Commands
+{
+    public record InsertPersonCommand(string FirstName, string LastName) : IRequest<PersonModel>;
+
+    //public class InsertPersonCommandClass : IRequest<PersonModel>
+    //{
+    //    public string _firstName { get; set; }
+    //    public string _lastName { get; set; }
+    //    public InsertPersonCommandClass(string firstName, string lastName)
+    //    {
+    //        _firstName = firstName;
+    //        _lastName = lastName;
+    //    }
+    //}
+}
---
+++ CQRS_MediatR_Pattern/MediatR_Demo/DemoDataAccess/Queries/GetPersonByIdQuery.cs
@@ -0,0 +1,25 @@
+﻿using DemoLibrary.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.Queries
+{
+    public record GetPersonByIdQuery(int id) : IRequest<PersonModel>;
+
+    //if we were to use a class instead of a record this is how it would be implemented:
+
+    //public class GetPersonByIdQueryClass : IRequest<PersonModel>
+    //{
+    //    public int _id { get; set; }
+
+    //    public GetPersonByIdQueryClass(int id)
+    //    {
+    //        _id = id;
+    //    }
+    //}
+
+}

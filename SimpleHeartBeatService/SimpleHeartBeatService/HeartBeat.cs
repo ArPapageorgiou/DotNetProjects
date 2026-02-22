067b6e9c38d20aaff3fdf9c332d0c5df792676da
Argiris Papageorgiou <ar.papageorgiou@gmail.com>
2026-02-22 12:41:54 +0200

    A simple heartbeat service that logs a message every second using Topshelf

---
+++ SimpleHeartBeatService/SimpleHeartBeatService/HeartBeat.cs
@@ -0,0 +1,29 @@
+﻿namespace SimpleHeartBeatService
+{
+    public class HeartBeat
+    {
+        private readonly System.Timers.Timer _timer;
+
+        public HeartBeat()
+        {
+            _timer = new System.Timers.Timer(1000) { AutoReset = true };
+            _timer.Elapsed += Timer_Elapsed;
+        }
+
+        private void Timer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+        {
+            string[] lines = new string[] {DateTime.Now.ToString()};
+            File.AppendAllLines(@"C:\Temporary\Demos\Heartbeat.txt", lines);
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
---
+++ SimpleHeartBeatService/SimpleHeartBeatService/Program.cs
@@ -0,0 +1,30 @@
+﻿using Topshelf;
+
+namespace SimpleHeartBeatService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<HeartBeat>(s =>
+                {
+                    s.ConstructUsing(heartbeat => new HeartBeat());
+                    s.WhenStarted(heartbeat => heartbeat.Start());
+                    s.WhenStopped(heartbeat => heartbeat.Stop());
+                });
+
+                x.RunAsLocalSystem();
+
+                x.SetDescription("A simple heartbeat service that logs a message every second.");
+                x.SetDisplayName("Simple Heartbeat Service");
+                x.SetServiceName("SimpleHeartBeatService"); 
+                x.StartAutomatically();
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
